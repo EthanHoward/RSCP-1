d68287857c1bcf61ed0fb45b708bae3543319034
EthanHoward <67965158+EthanHoward@users.noreply.github.com>
2021-10-19 20:00:31 +0100

    Delete AVAScript directory

--- AVAScript/Main.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-
-void log(std::string log){std::cout << "| LOG | " + log << std::endl;}
-void err(std::string err){std::cout << "| ERR | " + err << std::endl;}
-void dbg(std::string dbg) { std::cout << "| DBG | " + dbg << std::endl;}
